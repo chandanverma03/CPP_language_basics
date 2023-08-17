a98f3c910765d60dfcf9ec8a2e4ea050d54f794d
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-08-17 10:14:55 +0530

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 5;
+	cout<<"\n\n\t\tWelcome to CPP Programming\t\t\n\n";
+	
+	cout<<"Hello World !\t\t\n\n\n";
+	cout<<"The value of a is "<<a;
+	return 0;
+}
