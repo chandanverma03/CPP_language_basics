0ec39812f4fc28f2ae21e50a5d45d0ee9239e2ac
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-08-18 12:42:58 +0530

    Add files via upload

---
+++ taking_value_form_user.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"\n\n\n\t\tWelcome to CPP Programming \n\n\n";
+	
+	int a;
+	cout<<"Enter the Value of a : ";
+	cin>>a;
+	cout<<"\nThe value of a is : "<<a;
+	return 0;
+}
