4bc32fd1149a9ce57b6bd61a17124aa30ad9e932
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-08-18 12:44:44 +0530

    Add files via upload

---
+++ headerfiles.cpp
@@ -0,0 +1,10 @@
+//There are two types of header files : 
+//1.System header files : It comes with the compiler   eg
+ #include<iostream>
+//User defined header files : It is written by the programmer  eg. 
+//#include"this.h"  it will give an error if it is not present in current directory  
+using namespace std;
+int main(){
+	cout<<"\n\n\n\t\tWelcome to CPP Programming\n\n\n ";
+	
+}
