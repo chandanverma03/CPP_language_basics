7e65871a21bfd1effcbfb7d98684dff2f2b13f80
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-10-15 23:56:40 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string userName;
+	cout<<"\n\n\n\t\tWelcome to CPP Programming\n\n\n";
+    cout<<"Please enter your name: ";
+    cin>>userName;
+
+    cout<< "Hello, "<<userName<<"! Welcome to the CPP program."<<endl;
+
+    return 0;
+}
+
